5574ba679469c384111b76c4ff71b64bd780703a
Huto <92351427+Hutotpn@users.noreply.github.com>
2022-11-13 03:57:58 +0000

    Working on

--- My Info/script.js
+++
@@ -1,7 +0,0 @@
-const datajs = {
-    "id": 1,
-    "name": "John Doe",
-    "age": 15
-}
-
-document.getElementById("datasj").innerHTML = datajs.name;
---
+++ Website/script.js
@@ -0,0 +1,17 @@
+const myData = {
+    "name": "Name: My Info",
+    "nickname": "Nickname: Info",
+    "email": "Email: contact.hutotpn@gmail.com",
+    "phoneNumber": "Phone number: +9999 123-456-789",
+    "age": "Age: " + 15,
+    "fb": "test"
+
+}
+
+document.getElementById("dataName").innerHTML = myData.name;
+document.getElementById("dataNickname").innerHTML = myData.nickname;
+document.getElementById("dataEmail").innerHTML = myData.email;
+document.getElementById("dataPhone").innerHTML = myData.phoneNumber;
+document.getElementById("dataAge").innerHTML = myData.age;
+document.getElementById("dataFacebook").innerHTML = myData.fb;
+document.getElementById("dataTwitter").innerHTML = myData.nickname;
