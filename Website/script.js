681af097278a2cae083905e815183f3e8bc72818
Huto <92351427+Hutotpn@users.noreply.github.com>
2022-11-20 07:06:16 +0700

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -4,14 +4,24 @@ const myData = {
     "email": "Email: contact.hutotpn@gmail.com",
     "phoneNumber": "Phone number: +9999 123-456-789",
     "age": "Age: " + 15,
-    "fb": "test"
+    "twitterHandle": "Twitter: @Twitter",
+    "youtubeHandle": "YouTube: @YouTube",
 
+    "twitterLink": "//twitter.com/twitter",
+    "youtubeLink": "//youtube.com/@YouTube"
 }
 
-document.getElementById("dataName").innerHTML = myData.name;
-document.getElementById("dataNickname").innerHTML = myData.nickname;
-document.getElementById("dataEmail").innerHTML = myData.email;
-document.getElementById("dataPhone").innerHTML = myData.phoneNumber;
-document.getElementById("dataAge").innerHTML = myData.age;
-document.getElementById("dataFacebook").innerHTML = myData.fb;
-document.getElementById("dataTwitter").innerHTML = myData.nickname;
+// Change text
+    document.getElementById("dataName").innerHTML = myData.name;
+    document.getElementById("dataNickname").innerHTML = myData.nickname;
+    document.getElementById("dataEmail").innerHTML = myData.email;
+    document.getElementById("dataPhone").innerHTML = myData.phoneNumber;
+    document.getElementById("dataAge").innerHTML = myData.age;
+    document.getElementById("dataTwitter").innerHTML = myData.twitterHandle;
+    document.getElementById("dataYoutube").innerHTML = myData.youtubeHandle;
+// End Change text
+
+// Change Hyperlink
+    document.getElementById("dataTwitter").href = myData.twitterLink;
+    document.getElementById("dataYoutube").href = myData.youtubeLink;
+// End Change Hyperlink
